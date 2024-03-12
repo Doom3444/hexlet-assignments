b850f24cff4827003ac466ff9b38ecf84e0c55d0
@hexlet/cli <support@hexlet.io>
2024-03-12 17:24:34 +0300

    submit java-oop-ru/patterns

---
+++ java-oop-ru/patterns/src/main/java/exercise/InputTag.java
@@ -0,0 +1,23 @@
+package exercise;
+
+// BEGIN
+class InputTag implements TagInterface {
+
+    private String type;
+    private String value;
+
+    InputTag(String type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public String render() {
+        return "<input type=\"" +
+                type +
+                "\" value=\"" +
+                value +
+                "\">";
+    }
+}
+// END
---
+++ java-oop-ru/patterns/src/main/java/exercise/LabelTag.java
@@ -0,0 +1,22 @@
+package exercise;
+
+// BEGIN
+class LabelTag implements TagInterface {
+
+    private String label;
+    private TagInterface tag;
+
+    LabelTag(String label, TagInterface tag) {
+        this.label = label;
+        this.tag = tag;
+    }
+
+    @Override
+    public String render() {
+        return "<label>" +
+                label +
+                tag.render() +
+                "</label>";
+    }
+}
+// END
