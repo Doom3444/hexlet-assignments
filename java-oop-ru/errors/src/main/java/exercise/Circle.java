a004e9b239d900d72d357d478276f10515c6cf5c
@hexlet/cli <support@hexlet.io>
2024-03-13 16:27:26 +0300

    submit java-oop-ru/errors

---
+++ java-oop-ru/errors/src/main/java/exercise/Circle.java
@@ -0,0 +1,25 @@
+package exercise;
+
+// BEGIN
+public class Circle {
+
+    Point point;
+    int radius;
+
+    public Circle(Point point, int radius) {
+        this.point = point;
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public double getSquare() throws NegativeRadiusException {
+        if (radius < 0) {
+            throw new NegativeRadiusException("Invalid radius");
+        }
+        return Math.PI * radius * radius;
+    }
+}
+// END
